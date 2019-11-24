942f57e4fc068200dbd9f4a9e2debbae3e190d06
Joshua Pan <joshuapan8@gmail.com>
2019-11-23 23:25:36 -0500

    Update shiny app with NBA position model

--- nba_college_stat_analysis/server.R
+++ nba_college_stat_analysis/server.R
@@ -13,10 +13,15 @@ nba_total_stats <- read_rds("nba_total_stats.rds")
 nba_total_stats_wo_tot <- read_rds("nba_total_stats_wo_tot.rds")
 
 shinyServer(function(input, output) {
-  output$generalTrends <- renderPlot({
+
+  ######################
+  ### General trends ###
+  ######################
+
+  output$generalTrend.plot <- renderPlot({
     college_plot <- college_totals_stats %>%
       group_by(season) %>%
-      summarize(var_avg = mean(get(input$stat), na.rm = TRUE)) %>%
+      summarize(var_avg = mean(get(input$generalTrend.stat), na.rm = TRUE)) %>%
       replace_na(replace = list(var_avg = 0)) %>%
       ggplot() +
       geom_point(aes(x = season, y = var_avg)) +
@@ -25,7 +30,7 @@ shinyServer(function(input, output) {
 
     nba_plot <- nba_total_stats_wo_tot %>%
       group_by(season) %>%
-      summarize(var_avg = mean(get(input$stat), na.rm = TRUE)) %>%
+      summarize(var_avg = mean(get(input$generalTrend.stat), na.rm = TRUE)) %>%
       replace_na(replace = list(var_avg = 0)) %>%
       ggplot() +
       geom_point(aes(x = season, y = var_avg)) +
@@ -33,5 +38,36 @@ shinyServer(function(input, output) {
       theme_few()
 
     plot_grid(college_plot, nba_plot, nrow = 2, ncol = 1)
+  })
+
+  ##########################
+  ### Position modelling ###
+  ##########################
+
+  positionModel.positions <- reactive({
+    cat("computing positions\n")
+
+    joined_college_stats_nba_position %>%
+      mutate(pos_binary = as.factor(ifelse(pos == input$positionModel.position, "1", "0"))) %>%
+      filter(!is.na(get(input$positionModel.stat)))
+  })
+
+  output$positionModel.plot <- renderPlot({
+    cat("try rendering?\n")
+
+    ggplot(positionModel.positions(), aes(x = pos_binary, y = get(input$positionModel.stat))) +
+      geom_boxplot()
+  })
+  
+  output$positionModel.accuracy <- renderTable({
+    cat("accuracy?\n")
+
+    model <- glm(data = positionModel.positions(), formula = pos_binary ~ get(input$positionModel.stat), family = "binomial")
+    
+    pred <- positionModel.positions() %>%
+      mutate(prediction = predict(model, type = "response")) %>%
+      mutate(pred_binary = as.factor(ifelse(prediction > mean(prediction), "1", "0")))
+    
+    metrics(pred, pos_binary, pred_binary)
   })
 })
--- nba_college_stat_analysis/ui.R
+++ nba_college_stat_analysis/ui.R
@@ -2,8 +2,19 @@ library(shiny)
 library(shinythemes)
 library(markdown)
 
-stats <- c("g", "gs", "mp", "fg", "fga", "fg_pct", "fg2", "fg2a", "fg2_pct", "fg3", "fg3a",
-           "fg3_pct", "ft", "fta", "ft_pct", "orb", "drb", "trb", "ast", "stl", "blk", "tov", "pf", "pts")
+stats <- c(
+  "g", "gs", "mp", "fg", "fga", "fg_pct", "fg2", "fg2a", "fg2_pct", "fg3", "fg3a",
+  "fg3_pct", "ft", "fta", "ft_pct", "orb", "drb", "trb", "ast", "stl", "blk", "tov", "pf", "pts"
+)
+
+stats_per_g <- c(
+  "g", "gs", "mp_per_g", "fg_per_g", "fga_per_g", "fg_pct", "fg2_per_g", "fg2a_per_g",
+  "fg2_pct", "fg3_per_g", "fg3a_per_g", "fg3_pct", "ft_per_g", "fta_per_g", "ft_pct",
+  "orb_per_g", "drb_per_g", "trb_per_g", "ast_per_g", "stl_per_g", "blk_per_g", "tov_per_g",
+  "pf_per_g", "pts_per_g"
+)
+
+positions <- c("C", "SG", "PF", "PG", "SF")
 
 generalTrends <- tabPanel(
   "General Trends",
@@ -13,9 +24,10 @@ generalTrends <- tabPanel(
       h3("What are some trends for the NBA and NCAA over seasons?"),
 
       # Get statistic name to plot general trend
+
       sidebarPanel(
         selectInput(
-          inputId = "stat",
+          inputId = "generalTrend.stat",
           label = "Statistic:",
           choices = stats,
           selected = "fg3"
@@ -23,9 +35,49 @@ generalTrends <- tabPanel(
       ),
 
       # Plot the general trend for selected statistic
+
       mainPanel(
-        plotOutput("generalTrends")
+        plotOutput("generalTrend.plot"),
+        br(),
+        p("Notice a large dip in many statistics during the NBA season 1998-1999. This was because of the 1999 NBA lockout. You can read more about it here: https://en.wikipedia.org/wiki/1998%E2%80%9399_NBA_season.")
       )
+    )
+  )
+)
+
+positionModel <- tabPanel(
+  "NBA Position Model",
+  tabsetPanel(
+    tabPanel(
+      "Try it Yourself!",
+      h3("Can we predict a player's NBA position from one of their statistics?"),
+
+      # Get statistic name and position of player
+
+      sidebarPanel(
+        selectInput(
+          inputId = "positionModel.position",
+          label = "Position:",
+          choices = positions,
+          selected = "C"
+        ),
+        selectInput(
+          inputId = "positionModel.stat",
+          label = "Statistic:",
+          choices = stats_per_g,
+          selected = "blk_per_g"
+        )
+      ),
+
+      # Plot the boxplot for the position vs. stat
+
+      mainPanel(
+        plotOutput("positionModel.plot"),
+        tableOutput("positionModel.accuracy")
+      )
+    ),
+    tabPanel(
+      "General Findings"
     )
   )
 )
@@ -37,9 +89,7 @@ shinyUI(fluidPage(
   navbarPage(
     "Dunk on Some Stats",
     generalTrends,
-    tabPanel(
-      "NBA Position Model"
-    ),
+    positionModel,
     tabPanel(
       "Fun Facts!"
     ),
