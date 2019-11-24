e3d88292a477b7e64a4e5225a6776ecf9d3c059e
Joshua Pan <joshuapan8@gmail.com>
2019-11-23 22:25:55 -0500

    Bring necessary modelling data into shiny app

--- csv_to_rds.R
+++ csv_to_rds.R
@@ -15,3 +15,21 @@ write_rds(nba_per_game_stats, "nba_college_stat_analysis/nba_per_game_stats.rds"
 write_rds(nba_per_game_stats_wo_tot, "nba_college_stat_analysis/nba_per_game_stats_wo_tot.rds")
 write_rds(nba_total_stats, "nba_college_stat_analysis/nba_total_stats.rds")
 write_rds(nba_total_stats_wo_tot, "nba_college_stat_analysis/nba_total_stats_wo_tot.rds")
+
+## do some more work for joined NBA positions and college stat table
+
+nba_single_position <- read_csv("final_csv/nba_single_position.csv")
+
+stats_per_g <- c("g", "gs", "mp_per_g", "fg_per_g", "fga_per_g", "fg_pct", "fg2_per_g", "fg2a_per_g", "fg2_pct", "fg3_per_g", "fg3a_per_g",
+                 "fg3_pct", "ft_per_g", "fta_per_g", "ft_pct", "orb_per_g", "drb_per_g", "trb_per_g", "ast_per_g", "stl_per_g", "blk_per_g", "tov_per_g", "pf_per_g", "pts_per_g")
+
+average_college_per_game_stats <- college_per_game_stats %>%
+  select(-name, -college_slug, -season, -school_name, -conf_abbr) %>%
+  group_by(slug) %>%
+  summarise_all(partial(mean, na.rm = TRUE))
+
+joined_college_stats_nba_position <- average_college_per_game_stats %>%
+  left_join(nba_single_position, by = "slug") %>%
+  filter(!is.na(pos))
+
+write_rds(joined_college_stats_nba_position, "nba_college_stat_analysis/joined_college_stats_nba_position.rds")
