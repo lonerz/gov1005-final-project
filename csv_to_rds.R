8505b29e739d5e022afab865d0bbed8b32575f60
Joshua Pan <joshuapan8@gmail.com>
2019-11-23 22:28:54 -0500

    Update csv_to_rds with comments and formatting

--- csv_to_rds.R
+++ csv_to_rds.R
@@ -20,13 +20,22 @@ write_rds(nba_total_stats_wo_tot, "nba_college_stat_analysis/nba_total_stats_wo_
 
 nba_single_position <- read_csv("final_csv/nba_single_position.csv")
 
-stats_per_g <- c("g", "gs", "mp_per_g", "fg_per_g", "fga_per_g", "fg_pct", "fg2_per_g", "fg2a_per_g", "fg2_pct", "fg3_per_g", "fg3a_per_g",
-                 "fg3_pct", "ft_per_g", "fta_per_g", "ft_pct", "orb_per_g", "drb_per_g", "trb_per_g", "ast_per_g", "stl_per_g", "blk_per_g", "tov_per_g", "pf_per_g", "pts_per_g")
+stats_per_g <- c(
+  "g", "gs", "mp_per_g", "fg_per_g", "fga_per_g", "fg_pct", "fg2_per_g", "fg2a_per_g",
+  "fg2_pct", "fg3_per_g", "fg3a_per_g", "fg3_pct", "ft_per_g", "fta_per_g", "ft_pct",
+  "orb_per_g", "drb_per_g", "trb_per_g", "ast_per_g", "stl_per_g", "blk_per_g", "tov_per_g",
+  "pf_per_g", "pts_per_g"
+)
+
+# Compute the average per_game stats for each player. So group by the slug (unique identifier)
+# for each player and then use summarise_all to summarize all the columns
 
 average_college_per_game_stats <- college_per_game_stats %>%
   select(-name, -college_slug, -season, -school_name, -conf_abbr) %>%
   group_by(slug) %>%
   summarise_all(partial(mean, na.rm = TRUE))
+
+# Join the college stats with NBA positions based on slug (unique indentifier)
 
 joined_college_stats_nba_position <- average_college_per_game_stats %>%
   left_join(nba_single_position, by = "slug") %>%
